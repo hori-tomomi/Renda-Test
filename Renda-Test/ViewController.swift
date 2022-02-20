86d01616ba2b7bccdd3a68f1e446e28213eca8bc
TomomiHori <hr.tmm.23@icloud.com>
2022-02-21 02:38:16 +0900

    ・UI改善 ・コード改善 ・リスタート実装 ・画像差し替え

--- Renda-Test/ViewController.swift
+++ Renda-Test/ViewController.swift
@@ -15,89 +15,83 @@ class ViewController: UIViewController {
     @IBOutlet var buildingImageView: UIImageView!
     @IBOutlet var tapButton: UIButton!
     @IBOutlet var startButton: UIButton!
-    @IBOutlet var finishButton: UIButton!
     @IBOutlet var resultLabel: UILabel!
-   
-    
+    @IBOutlet var buildingNameLabel: UILabel!
     
     var tapCount = 0
-    var levelArray:[String] = ["Level1", "Level2", "Level3", "Level4", "Level5", "Level6"]
     var changeNo = 0
     var timeCount: Float = 0.0
     var timer:Timer = Timer()
     
-   // var buildingsArray:[String] = ["building1", "building2", "buiding3", "building4"]
+    var levelArray:[String] = ["Level1", "Level2", "Level3", "Level4", "Level5", "Level6"]
+    var buildingArray:[String] = ["Building1", "Building2", "Building3", "Building4","Building5"]
+    var buildingNameArray:[String] = ["京都タワー　131m", "ツインアーチ　138m", "神戸ポートタワー 108m", "福岡タワー　234m", "東京タワー　333m"]
+    
+    var isBuilding: Bool = false;
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        resultLabel.isHidden = true
        
     }
     
     @IBAction func tapTapButton() {
         
-        if tapCount < 51{
-            tapCount = tapCount + 1
-        }
-        
         countLabel.text = String(tapCount)
         levelLabel.text = String(levelArray[changeNo])
+        buildingImageView.image = UIImage(named: String(buildingArray[changeNo]))
+        buildingNameLabel.text = String(buildingNameArray[changeNo])
         
-        
-        if tapCount < 10{
-            buildingImageView.image = UIImage(named: "Building1")
-        } else if tapCount < 20{
-            changeNo = 1
-            buildingImageView.image = UIImage(named: "Building2")
-        } else if tapCount < 30{
-            changeNo = 2
-            buildingImageView.image = UIImage(named: "Building3")
-        } else if tapCount < 40{
-            changeNo = 3
-            buildingImageView.image = UIImage(named: "Building4")
-        } else if tapCount < 51{
-            changeNo = 4
-            buildingImageView.image = UIImage(named: "Building5")
-        } else if tapCount < 60{
-            changeNo = 5
-            buildingImageView.image = UIImage(named: "Building6")
+        if isBuilding == false {
+            return
         }
         
+        tapCount = tapCount + 1
         
-        
+        if tapCount < 10{
+        } else if tapCount < 20{
+            changeNo = 1
+        } else if tapCount < 30{
+            changeNo = 2
+        } else if tapCount < 50{
+            changeNo = 3
+        } else if tapCount < 51{
+            changeNo = 4
+        } else {
+            isBuilding = false
+            finish()
+        }
     }
     
     @IBAction func start() {
+        isBuilding = true
+        timeCount = 0
+        tapCount = 0
+        changeNo = 0
+        
+        buildingImageView.image = UIImage(named: String(buildingArray[changeNo]))
+        buildingNameLabel.text = String(buildingNameArray[changeNo])
+        resultLabel.isHidden = true
         
         if !timer.isValid {
-            
             timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.up), userInfo: nil, repeats: true)
-            
         }
-     
-    }
-    
-    @IBAction func finish() {
-        
-        if timer.isValid {
-            timer.invalidate()
-        }
-        result()
-        
     }
     
     @objc func up(){
         timeCount = timeCount + 0.01
-        timeCountLabel.text = String(format: "%.2f", timeCount)
+        timeCountLabel.text = String(format: "%.2f", timeCount) + "year"
     }
     
-    func result() {
+    func finish() {
+        if timer.isValid {
+            timer.invalidate()
+        }
+        resultLabel.isHidden = false
         resultLabel.text = String(format: "%.2f", timeCount) + "年かかりました！"
     }
-
-
 }
 
